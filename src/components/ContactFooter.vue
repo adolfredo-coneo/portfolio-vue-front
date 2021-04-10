53722ed1073242d547b003d0d145f92015977adf
Adolfredo Coneo <ado.coneo@gmail.com>
2021-04-09 21:31:39 -0500

    ContactFooter

--- src/components/ContactFooter.vue
+++ src/components/ContactFooter.vue
@@ -10,30 +10,80 @@
           <router-link to="/contact">Contact Me</router-link>
         </div>
       </div>
-      <div class="w-2/2 md:w-1/2 mb-4 md:mb-0 flex justify-center">
-        <ul>
-          <li>
-            <span>
-              <svg
-                class="h-8 w-8 text-red-500"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
-                />
-              </svg>
-            </span>
-            ado.coneo@gmail.com
-          </li>
-          <li>Linkedin</li>
-          <li>Twitter</li>
-          <li>Blog</li>
-        </ul>
+      <div class="w-2/2 md:w-1/2 mb-4 md:mb-0 justify-center grid grid-cols-2">
+        <div>
+          <span>
+            <svg
+              class="h-8 w-8 text-red-500"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+              />
+            </svg>
+          </span>
+          ado.coneo@gmail.com
+        </div>
+        <div>
+          <svg
+            class="h-8 w-8 text-red-500"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <path
+              d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"
+            />
+            <rect x="2" y="9" width="4" height="12" />
+            <circle cx="4" cy="4" r="2" />
+          </svg>
+          Linkedin
+        </div>
+        <div>
+          <svg
+            class="h-8 w-8 text-red-500"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <path
+              d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"
+            /></svg
+          >Twitter
+        </div>
+        <div>
+          <svg
+            class="h-8 w-8 text-red-500"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            stroke-width="2"
+            stroke="currentColor"
+            fill="none"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" />
+            <path
+              d="M16 6h3a1 1 0 0 1 1 1v11a2 2 0 0 1 -4 0v-13a1 1 0 0 0 -1 -1h-10a1 1 0 0 0 -1 1v12a3 3 0 0 0 3 3h11"
+            />
+            <line x1="8" y1="8" x2="12" y2="8" />
+            <line x1="8" y1="12" x2="12" y2="12" />
+            <line x1="8" y1="16" x2="12" y2="16" />
+          </svg>
+          Blog
+        </div>
       </div>
     </div>
   </div>
